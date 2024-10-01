1f221847abac9d47ef3008f063570a893c50f5ba
Skolldar <exoreiba@gmail.co>
2024-10-01 11:33:27 +0200

    Crear el protecto . hacer los estilos del css. agregar e importat module.css crear los componentes

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styles from "./App.module.css"
+const App = () => {
+  return (
+    <>
+    <h1 className={styles.tittle}>Buscador de Climas</h1>
+
+    <div className={styles.container}>
+      <p>1</p>
+      <p>2</p>
+    </div>
+    </>
+
+  )
+}
+
+export default App
