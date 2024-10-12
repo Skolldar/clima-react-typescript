254badd6fffa1d2255bf61eb03ed9c9117e66199
Skolldar <exoreiba@gmail.co>
2024-10-12 16:45:44 -0300

    importado el hook, alert,spinner, y el componente de weather detail...
    agregando el fetch al form...
    
    validando si hay algo en el weatherdeatil para que aparezca el texto o no..
    
    si no se consigue la ciudad dar un aviso

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import styles from "./App.module.css"
 import Form from "./components/Form/Form"
+import Spinner from "./components/Spinner/Spinner"
+import WeatherDetail from "./components/WeatherDetails/WeatherDetail"
+import useWeather from "./hooks/useWeather"
+import Alert from "./components/Alert/Alert"
+
 const App = () => {
+
+  const  {weather, loading, notFound, fetchWeather, hasWeatherData} = useWeather()
+
   return (
     <>
     <h1 className={styles.tittle}>Weather Search</h1>
 
     <div className={styles.container}>
-      <Form/>
-      <p>2</p>
+      <Form fetchWeather={fetchWeather}/>
+      {loading && <Spinner/>}
+      {hasWeatherData && <WeatherDetail weather={weather}/>}
+      {notFound && <Alert>City not found</Alert>}
+
     </div>
     </>
-
   )
 }
 
