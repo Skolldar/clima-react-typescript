2c1cabac8da138bab497e556ff486516d01d698f
Skolldar <exoreiba@gmail.co>
2025-04-01 20:29:22 +0200

    Added new country data to the countries.ts file

--- src/data/countries.ts
+++ src/data/countries.ts
@@ -2,19 +2,23 @@ import type { Country } from "../types/types-index"
 
 export const countries: Country[] = [
   { code: 'US', name: 'United States' },
-  { code: 'MX', name: 'México' },
-  { code: 'AR', name: 'Argentina' },
-  { code: 'CO', name: 'Colombia' },
-  { code: 'CR', name: 'Costa Rica' },
-  { code: 'ES', name: 'España' },
-  { code: 'PE', name: 'Perú' },
-  { code: 'BR', name: 'Brazil' },
-  { code: 'FR', name: 'France' },
-  { code: 'DE', name: 'Germany' },
-  { code: 'IT', name: 'Italy' },
-  { code: 'JP', name: 'Japan' },
-  { code: 'IN', name: 'India' },
-  { code: 'CA', name: 'Canada' },
   { code: 'GB', name: 'United Kingdom' },
-  { code: 'AU', name: 'Australia' }
-  ]
+  { code: 'CA', name: 'Canada' },
+  { code: 'AU', name: 'Australia' },
+  { code: 'DE', name: 'Germany' },
+  { code: 'FR', name: 'France' },
+  { code: 'IT', name: 'Italy' },
+  { code: 'ES', name: 'Spain' },
+  { code: 'JP', name: 'Japan' },
+  { code: 'CN', name: 'China' },
+  { code: 'IN', name: 'India' },
+  { code: 'BR', name: 'Brazil' },
+  { code: 'MX', name: 'Mexico' },
+  { code: 'ZA', name: 'South Africa' },
+  { code: 'AE', name: 'United Arab Emirates' },
+  { code: 'SG', name: 'Singapore' },
+  { code: 'KR', name: 'South Korea' },
+  { code: 'RU', name: 'Russia' },
+  { code: 'TR', name: 'Turkey' },
+  { code: 'SA', name: 'Saudi Arabia' }
+];
