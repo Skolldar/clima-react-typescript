bfeb39bf2cb7e28885803a01c06833e1dad06b8f
Skolldar <exoreiba@gmail.co>
2025-04-01 17:25:13 +0200

    i addded new countries

--- src/data/countries.ts
+++ src/data/countries.ts
@@ -1,11 +1,20 @@
 import type { Country } from "../types/types-index"
 
 export const countries: Country[] = [
-    { code: 'US', name: 'Estados Unidos' },
-    { code: 'MX', name: 'México' },
-    { code: 'AR', name: 'Argentina' },
-    { code: 'CO', name: 'Colombia' },
-    { code: 'CR', name: 'Costa Rica' },
-    { code: 'ES', name: 'España' },
-    { code: 'PE', name: 'Perú' }
+  { code: 'US', name: 'United States' },
+  { code: 'MX', name: 'México' },
+  { code: 'AR', name: 'Argentina' },
+  { code: 'CO', name: 'Colombia' },
+  { code: 'CR', name: 'Costa Rica' },
+  { code: 'ES', name: 'España' },
+  { code: 'PE', name: 'Perú' },
+  { code: 'BR', name: 'Brazil' },
+  { code: 'FR', name: 'France' },
+  { code: 'DE', name: 'Germany' },
+  { code: 'IT', name: 'Italy' },
+  { code: 'JP', name: 'Japan' },
+  { code: 'IN', name: 'India' },
+  { code: 'CA', name: 'Canada' },
+  { code: 'GB', name: 'United Kingdom' },
+  { code: 'AU', name: 'Australia' }
   ]
