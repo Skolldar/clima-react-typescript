79c6ffe7bff3f9c5e49c466cb532fdc7b0a531a7
Carla <exoreiba@email.com>
2026-02-02 16:37:28 +0100

    feat: improve styling and layout of WeatherSunTime component

--- src/components/WeatherDetails/WeatherSunTime.tsx
+++ src/components/WeatherDetails/WeatherSunTime.tsx
@@ -9,21 +9,21 @@ const WeatherSunTime = ({weather}: WeatherSunTimeProps) => {
   return (
     <>
     <div className="blur-card">
-          <h2 className="font-semibold text-primary text-left pb-4">Sunset and Sunrise</h2>
+          <h2 className="font-semibold text-primary text-left pb-5">Sunset and Sunrise</h2>
           <div className="mt-2 grid items-center gap-6 text-primary sm:grid-cols-2">
-          <div className="bg-white/50 shadow-md p-4 rounded-xl flex flex-col items-center">
+          <div className="bg-white/50 shadow-md p-8 rounded-xl flex flex-col items-center">
               <div className="mb-4 flex justify-center">
-                <img src="../sunrise.png" alt="Sunrise" className='w-30 h-30' />
+                <img src="../sunrise.png" alt="Sunrise" className='w-35 h-35' />
               </div>
-              <div className="text-primary text-md font-normal">
+              <div className="text-primary py-5 text-md font-normal">
                 <p className="text-2xl font-semibold">{format(new Date(weather.sys.sunrise * 1000), 'h:mm a')}</p>
               </div>
             </div>
-          <div className="bg-white/50 shadow-md p-4 rounded-xl flex flex-col items-center">
+          <div className="bg-white/50 shadow-md p-8 rounded-xl flex flex-col items-center">
               <div className="mb-4 flex justify-center">
-                <img src="../sunset.png" alt="Sunset" className='w-30 h-30' />
+                <img src="../sunset.png" alt="Sunset" className='w-35 h-35' />
               </div>
-              <div className="text-primary text-md font-normal">
+              <div className="text-primary py-5 text-md font-normal">
                 <p className="text-2xl font-semibold">{format(new Date(weather.sys.sunset * 1000), 'h:mm a')}</p>
               </div>
             </div>
